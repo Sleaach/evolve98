1b7f107e4ac9e730ce726d1de3ee792b22b477c2
Sleaach <139504259+Sleaach@users.noreply.github.com>
2024-04-26 02:01:39 +0300

    evolve98

--- evolve98/application/mac.py
+++
@@ -1,7 +0,0 @@
-import uuid
-def mac():
-    try:
-        mac = ':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff) for elements in range(5, -1, -1)])
-        print(f"MAC Adresi: {mac}")
-    except Exception as e:
-        print(f"Hata oluştu: {e}")
--- evolve98/setup.py
+++
@@ -1,2 +0,0 @@
-import os
-print(os.system("pip install -r library.txt"))
