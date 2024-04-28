dd20133583aa60f67718b68e7f8408b15714269b
Sleaach <139504259+Sleaach@users.noreply.github.com>
2024-04-28 17:19:51 +0300

    evolve98

--- evolve98/application/change_keyboard.py
+++
@@ -1,4 +0,0 @@
-import ctypes
-
-def change_keyboard_layout(layout_name):
-    ctypes.windll.user32.LoadKeyboardLayoutW(layout_name, 1)
--- evolve98/application/trash.py
+++
@@ -1,8 +0,0 @@
-import os
-def trash():
- pwdsorgu = input("Dizin yolu: ")
- dosyadi = input("Dosya adı: ")
-
- dosya_yolu = os.path.join(pwdsorgu, dosyadi)
-
- os.remove(dosya_yolu) 
